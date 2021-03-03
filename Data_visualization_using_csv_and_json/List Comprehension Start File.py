01bdbccabe12f15c39b151a5cb791c8222fc05b1
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-03 13:27:31 -0600

    class notes

--- Data_visualization_using_csv_and_json/List Comprehension Start File.py
+++ Data_visualization_using_csv_and_json/List Comprehension Start File.py
@@ -20,6 +20,9 @@ for i in old_list:
 
 # new_list = [expression(i) for i in old_list if filter(i)]
 
+#### list comprehension format    i = [expression, iteration, condition] $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
 
 #The list comprehension starts with a '[' and ']', to help you remember that the
 #result is going to be a list.
@@ -119,15 +122,24 @@ print(output)
 
 #6) Creating a list based on a condition
 
-
-
+new_range =  [i * i for i in range(5) if i % 2 == 0]
+print(new_range)
 #Output - [0, 4, 16]
 
 
 
 
 # 7) Extracting numbers only from a string and putting it in a list
+#NEW WAY
+string = "Hello 12345 World"
+numbers = [x for x in string if x.isdigit()]
+letters = [x for x in string if x.isalpha()]
+print(numbers)
 
+#old way:
+for x in string:
+    if x.isdigit():
+        print(x)
 
 
 # Output - ['1', '2', '3', '4', '5']
@@ -149,6 +161,10 @@ this is line5
 
 Save the file as test.txt '''
 
+thefile = open("test.txt", "r")
+
+result = [i for i in thefile if "line3" in i]
+print(result)
 
 
 
@@ -161,17 +177,20 @@ Save the file as test.txt '''
 
 # Create a function and name it double:
 
+def double(x):
+    return x*2
 
 # If you now just print that function with a value in it, it should look like this:
 
-
+print(double(10))
 
 # Answer - 20
 
 
 #We can easily use list comprehension on that function.
 
-
+i = [double(x) for x in range(10)]
+print(i)
 
 
 
@@ -184,7 +203,8 @@ Save the file as test.txt '''
 
 # 10) adding an IF condition to the above
 
-
+i = [double(x) for x in range(10) if x%2 == 0]
+print(i)
 
 
 
@@ -197,11 +217,11 @@ Save the file as test.txt '''
 
 # 11) You can add more arguments (using multiple iterators and lists):
 
+mylist = [x+y for x in [10,20,30] for y in [20,30,40]]
+print(mylist)
 
 
-
-
-# Output - [30, 50, 70, 50, 70, 90, 70, 90, 110]
+# Output - [30, 40, 50, 40, 50, 60, 50, 60, 70]
 
 
 
